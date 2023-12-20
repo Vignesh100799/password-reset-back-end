e7d2b19908c7d50da6afe92fb49d68632e7f833b
Vignesh100799 <baluvignesh422@gmail.com>
2023-12-20 19:28:58 +0530

    Complete

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const app = express();
 const URL = process.env.DB
 const secretKey = process.env.JWT_SECRET
 const PORT = 4000;
+const nodemailer = require("nodemailer");
+
 app.use(express.json());
 app.use(cors({
     origin: '*'
@@ -20,12 +22,6 @@ app.get('/', (req, res) => {
 app.post('/register', async (req, res) => {
     try {
         const { firstName, lastName, email, password, confirmPassword } = req.body
-        if (!firstName || !lastName || !email || !password || !confirmPassword) {
-            return res.status(400).json({ message: 'Please provide all required fields.' });
-        }
-        if (password !== confirmPassword) {
-            return res.status(400).json({ message: 'Passwords do not match.' });
-        }
         const hashedPassword = await bcrypt.hash(password, 10);
         const connection = await MongoClient.connect(URL)
         const db = connection.db("users")
@@ -56,18 +52,91 @@ app.post("/login", async (req, res) => {
         const user = await db.collection("Registered").findOne({ email })
 
         if (!user) {
-            res.status(404).json({ message: "User not match" })
+            res.status(404).json({ message: "User or password not match" })
         }
         const passwordValid = await bcrypt.compare(password, user.password)
         if (!passwordValid) {
-            res.status(404).json({ message: "password not match" })
+            res.status(404).json({ message: "user or password not match" })
         }
         const token = jsonwebtoken.sign({ userId: user._id }, secretKey, { expiresIn: "1h" })
         res.status(200).json({ message: 'Login successful', token });
         connection.close()
     } catch (error) {
-
+        console.log(error)
     }
+})
+
+app.post("/forget-password", async (req, res) => {
+    try {
+        const { email } = req.body
+        const connection = await MongoClient.connect(URL)
+        const db = connection.db("users")
+        const user = await db.collection("Registered").findOne({ email })
+
+        if (!user) {
+            res.status(404).json({ message: "User not registered" })
+        }
+        const token = Math.random().toString(36).substring(7)
+
+        await db.collection("Registered").updateOne({ email }, {
+            $set: {
+                token
+            }
+        })
+        connection.close()
+
+
+        const transporter = nodemailer.createTransport({
+            host: "smtp.office365.com",
+            port: 587,
+            secure: false,
+            auth: {
+                user: "dnelsona@outlook.com",
+                pass: process.env.OUTLOOK_PASSWORD,
+            },
+
+        });
+        const main = async () => {
+            try {
+                const info = transporter.sendMail({
+                    from: "dnelsona@outlook.com",
+                    to: email,
+                    subject: "Reset password link",
+                    text: `Click the following link to reset your password: http://reset-password/${token}`
+                });
+                res.status(200).json({ message: "Password reset link sent successfully." });
+
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ message: "Failed to send password reset email." });
+            }
+        };
+        await main();
+    } catch (error) {
+        console.log(error)
+    }
+})
+app.post("/reset-password/:token", async (req, res) => {
+ try {
+    const { password, confirmPassword } = req.body
+    const token = req.params.token
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const connection = await MongoClient.connect(URL)
+    const db = connection.db("users")
+    const user = await db.collection("Registered").findOne({ token :token})
+
+    await db.collection("Registered").updateOne({ token }, {
+        $set: {
+            password : hashedPassword,
+            confirmPassword:hashedPassword
+        }
+    })
+   connection.close()
+   res.send({message:"Password changed succesfully",user})
+ } catch (error) {
+    console.log(error)
+ }
+
 })
 app.listen(PORT, () => {
     console.log(`server started at http://localhost:${PORT} ...`);
